ade16da383e6fdcfe78053a812ecb1d0382ff685
StaticHex <indegon1@gmail.com>
2023-03-06 18:35:27 -0600

    Pushing refactor of main

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-from __future__         import print_function, division
-from obj_classes.pdfgen import PDFGen
+#from __future__         import print_function, division
+# from obj_classes.pdfgen import PDFGen
 
-generator = PDFGen(True)
-generator.writeOutPdf('./test.pdf')
+# generator = PDFGen(True)
+# generator.writeOutPdf('./test.pdf')
+"""
+Main driver class used to run pdf generation
+"""
+from __future__ import print_function, division
+from obj_classes.content_manager import ContentManager
+from obj_classes.pdf_generator import PDFGenerator
+from systems.fantasy import Fantasy
+import os
+
+if __name__ == "__main__":
+
+
+    # Define filenames and systems here
+    contentManagers = {
+         "linas_fantasy":Fantasy()
+    }
+
+    for system in contentManagers:
+        print(f'Writing out {system} to disk ...')
+        # Generate path to save file to
+        outFile = f"{os.getcwd()}/pdfs/{system}.pdf"
+
+        # Create pdf generator
+        generator = PDFGenerator(
+            outputPath=outFile,
+            cm=contentManagers[system],
+            debug=True
+        )
+
+        # Write out to file
+        generator.writeOutToPDF()
